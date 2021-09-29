8c29aecd4af368c86deb190bb0fa5e2f2b9464a4
Muhammet Yüce <yce.marqu@gmail.com>
2021-09-29 22:39:17 +0300

    Animation Control and humans added

---
+++ Assets/EscalatorManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscalatorManager : MonoBehaviour
+{
+    // Debug Purposes
+    [Range(0f,1f)]
+    public float animationPercentage = 0f;
+    
+    public bool isActive = false;
+    private Animator _anim;
+    private string ESCALATOR_ANIMATION_NAME = "EscalatorExpand";
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            _anim.Play(ESCALATOR_ANIMATION_NAME, 0, animationPercentage);
+        }
+    }
+}
