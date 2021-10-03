4bde7fffa056fcf58dfc066437de9c8bfc084b54
Muhammet Yüce <yce.marqu@gmail.com>
2021-10-03 13:07:30 +0300

    Operator Initializer Added

--- Assets/OperatorInitializer.cs
+++ Assets/OperatorInitializer.cs
@@ -1,27 +1,55 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class OperatorInitializer : MonoBehaviour
 {
     public Operator _operator;
 
+    [SerializeField] private GameObject Panel, AmountObject;
+
     private void Awake()
     {
-        if (_operator.operatorType == OperatorType.Multiplier)
+        string symbolForText;
+        symbolForText = HandleMultiplierAndDivisorDiff();
+
+        SetOnPanelText(symbolForText);
+        Scale();
+    }
+
+    private void Scale()
+    {
+        transform.localScale = new Vector3(_operator.size, 1f, 1f);
+    }
+
+    private void SetOnPanelText(string symbolForText)
+    {
+        if (symbolForText != null)
         {
-            setMaterial
-        }
-        else
-        {
-            
+            AmountObject.GetComponent<TextMeshPro>().SetText($"{_operator.amount}{symbolForText}");
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private string HandleMultiplierAndDivisorDiff()
     {
+        string textRep;
+        if (_operator.operatorType == OperatorType.Multiplier)
+        {
+            setMaterial(new Color(0, 0, 1, 0.33f));
+            textRep = "x";
+        }
+        else
+        {
+            setMaterial(new Color(1, 0, 0, 0.33f));
+            textRep = "÷";
+        }
+
+        return textRep;
         
+        void setMaterial(Color color)
+        {
+            Panel.GetComponent<Renderer>().material.SetColor("_Color",color);
+        }
     }
+    
 }
